20610f4dbc25f241ff631b077979ef38b6bb3620
Bryan Powell <bryan@metabahn.com>
2020-05-16 18:08:44 -0700

    Document an experiment

--- lib/pakyow/buildpack/base/steps/install_ruby.rb
+++ lib/pakyow/buildpack/base/steps/install_ruby.rb
@@ -21,6 +21,10 @@ module Pakyow
             "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby/#{@buildpack.config.stack}/ruby-#{@buildpack.config.ruby_version}.tgz"
           end
 
+          # This was an experiment in compiling ruby. It works, but is slow on first deploy. Revisit
+          # when we can maintain compiled ruby versions for common runtimes. Installing would amount
+          # to a simple download and extract, just like we do with official Heroku versions.
+          #
           # def perform
           #   if @buildpack.config.cached_ruby.exist?
           #     FileUtils.mkdir_p("vendor/ruby-#{@buildpack.config.ruby_version}")
@@ -29,12 +33,12 @@ module Pakyow
           #     system "git clone https://github.com/rbenv/ruby-build.git"
           #     system "PREFIX=#{@buildpack.config.vendor_path.join("ruby-build")} ./ruby-build/install.sh"
           #     system "rm -r ruby-build"
-
+          #
           #     system "#{ruby_build_options} vendor/ruby-build/bin/ruby-build #{@buildpack.config.ruby_version} #{@buildpack.config.ruby_install_path}"
           #     system "rm -r vendor/ruby-build"
           #   end
           # end
-
+          #
           # private def ruby_build_options
           #   [
           #     "RUBY_CONFIGURE_OPTS='--disable-install-doc'",
